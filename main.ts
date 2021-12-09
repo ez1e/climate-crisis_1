75f121bcfd93bdd5c189d344df62ff2933014e9d
ez1e <81249142+ez1e@users.noreply.github.com>
2021-12-08 22:04:03 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
-tiles.setTilemap(tilemap`level1`)
-let emit = sprites.create(assets.image`myImage`, SpriteKind.Player)
-scene.cameraFollowSprite(emit)
-tiles.placeOnRandomTile(emit, assets.tile`teleporter`)
-emit.setVelocity(1, 1)
-controller.moveSprite(emit)
+function level2Setup () {
+    tiles.setTilemap(tilemap`level1`)
+    emit = sprites.create(assets.image`myImage`, SpriteKind.Player)
+    scene.cameraFollowSprite(emit)
+    tiles.placeOnRandomTile(emit, assets.tile`teleporter`)
+    emit.setVelocity(1, 1)
+    controller.moveSprite(emit)
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`end-tile`, function (sprite, location) {
+    nextLevel()
+})
+function nextLevel () {
+    game.over(true)
+}
+function level1Setup () {
+    tiles.setTilemap(tilemap`level1`)
+    emit = sprites.create(assets.image`myImage`, SpriteKind.Player)
+    scene.cameraFollowSprite(emit)
+    tiles.placeOnRandomTile(emit, assets.tile`teleporter`)
+    emit.setVelocity(1, 1)
+    controller.moveSprite(emit)
+}
+let emit: Sprite = null
+level1Setup()
