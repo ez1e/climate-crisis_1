356d322c3e8c06fac9d45e1ed1996be6358814ab
ez1e <81249142+ez1e@users.noreply.github.com>
2021-12-08 22:46:26 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 function level2Setup () {
-    tiles.setTilemap(tilemap`level0`)
+    tiles.setTilemap(tilemap`l2`)
     tiles.placeOnRandomTile(emit, assets.tile`teleporter`)
     emit.setVelocity(1, 1)
 }
@@ -11,7 +11,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`end-tile`, function (sprite, 
     level2Setup()
 })
 function level1Setup () {
-    tiles.setTilemap(tilemap`level1`)
+    tiles.setTilemap(tilemap`l1`)
     emit = sprites.create(assets.image`myImage`, SpriteKind.Player)
     scene.cameraFollowSprite(emit)
     tiles.placeOnRandomTile(emit, assets.tile`teleporter`)
