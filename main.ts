388a01e6122cd7bccf73a76fa80537067fe545bf
ez1e <81249142+ez1e@users.noreply.github.com>
2021-12-08 21:33:56 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 tiles.setTilemap(tilemap`level1`)
 let emit = sprites.create(assets.image`myImage`, SpriteKind.Player)
 scene.cameraFollowSprite(emit)
+tiles.placeOnRandomTile(emit, assets.tile`teleporter`)
+emit.setVelocity(1, 1)
 controller.moveSprite(emit)
